2050cdb8a4e6857ac838ed7c5c6d37fe54a327c5
xinghengstar <xinghengstar@gmail.com>
2025-07-26 21:41:37 +0800

    fixed：后端初始化

---
+++ xh-ai-code-backend/src/main/java/com/xh/xhaicode/XhAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.xh.xhaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhAiCodeApplication.class, args);
+    }
+
+}
---
+++ xh-ai-code-backend/src/main/java/com/xh/xhaicode/common/PageRequest.java
@@ -0,0 +1,30 @@
+package com.xh.xhaicode.common;
+
+import lombok.Data;
+
+/**
+ * 通用分页请求类
+ */
+@Data
+public class PageRequest {
+
+    /**
+     * 当前页号
+     */
+    private int pageNum = 1;
+
+    /**
+     * 页面大小
+     */
+    private int pageSize = 10;
+
+    /**
+     * 排序字段
+     */
+    private String sortField;
+
+    /**
+     * 排序顺序（默认降序）
+     */
+    private String sortOrder = "descend";
+}
