de6521e7f8f538d3ef701d5851ce2e8ac6bd487d
xinghengstar <xinghengstar@gmail.com>
2025-07-28 06:39:30 +0800

    fixed：制造冲突v2

--- xh-ai-code-backend/src/main/java/com/xh/xhaicode/controller/HealthController.java
+++ xh-ai-code-backend/src/main/java/com/xh/xhaicode/controller/HealthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/check")
     public BaseResponse<String> checkHeath(){
-        return ResultUtils.success("ok1");
+        return ResultUtils.success("ok3");
     }
 
 }
