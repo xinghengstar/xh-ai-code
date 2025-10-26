d2ded67376a81436004540a1711c6e2859877777
xinghengstar <xinghengstar@gmail.com>
2025-10-27 07:04:52 +0800

    feat: 基础环境搭建

---
+++ xh-ai-code-backend/src/main/java/com/xh/xhaicode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.xh.xhaicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.xh.xhaicode.model.entity.User;
+
+/**
+ *  映射层。
+ *
+ * @author <a href="https://github.com/xinghengstar">星恒</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
