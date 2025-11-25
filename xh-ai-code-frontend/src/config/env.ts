20f3bd22bbbbc23d2e0805f765779ed0f95e4308
xinghengstar <xinghengstar@gmail.com>
2025-11-25 20:29:50 +0800

    feat：依赖添加

--- xh-ai-code-frontend/src/config/env.ts
+++ xh-ai-code-frontend/src/config/env.ts
@@ -1,3 +1,5 @@
+import { CodeGenTypeEnum } from '@/utils/codeGenTypes.ts'
+
 /**
  * 环境变量配置
  */
@@ -17,5 +19,10 @@ export const getDeployUrl = (deployKey: string) => {
 
 // 获取静态资源预览URL
 export const getStaticPreviewUrl = (codeGenType: string, appId: string) => {
-  return `${STATIC_BASE_URL}/${codeGenType}_${appId}/`
+  const baseUrl = `${STATIC_BASE_URL}/${codeGenType}_${appId}/`
+  // 如果是 vue 项目，浏览地址需要添加 dist 后缀
+  if (codeGenType === CodeGenTypeEnum.VUE_PROJECT) {
+    return `${baseUrl}dist/index.html`
+  }
+  return baseUrl
 }
